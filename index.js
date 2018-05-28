9fbf998734c3b8d26e259d2de7160d437cd1c35b
chaimoosh <chaimoosh@gmail.com>
2018-05-28 17:54:47 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+let store = {drivers: [], passengers: [], trips: []}
+
+let userId = 0
+
+class Driver {
+  constructor(name) {
+    this.id = ++userId
+    this.name = name
+    store.drivers.push(this)
+  }
+}
+
+let passengerId = 0
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+  }
+}
+
+let tripId = 0
+class Trip {
+  constructor(driver, passenger){
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    store.trips.push(this)
+  }
+  passenger() {
+    return store.passengers.find(function(thing){
+      return thing.id === this.passengerId
+    })
+  }
+  driver(){
+    return store.drivers.find(function(thing){
+      return thing.id === this.driverId
+    })
+  }
+}
